fd6a97e9b48b50bb729247da2547c5aab8617aed
craigs <hi@craigs.io>
2020-03-04 22:18:58 +1100

    Add support for es6 and loading nurses csv

---
+++ src/rostering/NurseRosterer/__tests__/index.test.js
@@ -0,0 +1,6 @@
+import {NurseRosterer} from '../index'
+
+it('builds a roster from available nurse list', () => {
+  expect(NurseRosterer).toBeDefined()
+  expect('You should write some tests for the NurseRosterer.').toHaveLength(50)
+})
